ec717beee09170b76f13437ec739753ee09c091c
algizgv-max <algizgv@gmail.com>
2025-11-12 10:53:05 +0500

    Delete WWW directory

--- WWW/index.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector(".my_button");
-
-btn.addEventListener("click",button_click);
-
-function button_click(){
-    
-    document.querySelector("body").classList.toggle("body_style");
-    document.querySelector(".feature").classList.toggle("featere_new");
-}
